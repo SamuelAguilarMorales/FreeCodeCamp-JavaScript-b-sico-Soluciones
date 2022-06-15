ed5eb29f46cc90e86e04464a791ccc3341cde2c2
Samuel Aguilar Morales <83357763+SamuelAguilarMorales@users.noreply.github.com>
2022-06-14 20:41:58 -0500

    Add files via upload

---
+++ freebootcamp.js
@@ -0,0 +1,373 @@
+// -------------------------- Comenta tu código de JavaScript
+
+//Samuel Aguilar Morales
+/*
+Comentario extendido
+*/
+
+//-------------------------- Declara variables de JavaScript
+
+var myName;
+
+//--------------------------- Almacenar valores con el operador de asignación
+
+// Configuración
+var a;
+a = 7;
+
+// Cambia solo el código debajo de esta línea
+
+//-------------------------- Asigna el valor de una variable a otra variable
+
+// Configuración
+var a;
+a = 7;
+var b;
+b = a;
+
+// Cambia solo el código debajo de esta línea
+
+//--------------------------- Inicializa variables con el operador de asignación
+
+var a = 9;
+
+//---------------------------- Declara variables de cadena
+
+var myFirstName = "Samuel";
+var myLastName = "Aguilar Morales"; 
+
+//---------------------------- Comprendiendo las variables no inicializadas
+
+// Cambia solo el código debajo de esta línea
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Cambia solo el código encima de esta línea
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// ----------------------------- Comprendiendo la sensibilidad de mayúsculas en las variables
+
+// Declaración de variables
+var studlyCapVar ;
+var properCamelCase;
+var titleCaseOver ;
+
+// Asignación de variables
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// -------------------------------- Explora las diferencias entre las palabras claves var y let
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//--------------------------------- Declara una variable de solo lectura con la palabra clave const
+
+const FCC = "freeCodeCamp"; // Cambia esta línea
+let fact = "is cool!"; // Cambia esta línea
+fact = "is awesome!";
+console.log(FCC, fact); // Cambia esta línea
+
+//-------------------------------- Suma dos números con JavaScript
+
+const sum = 10 + 10;
+
+//-------------------------------- Resta un número de otro con JavaScript
+
+const difference = 45 - 33;
+
+//-------------------------------- Multiplica dos números con JavaScript
+
+const product = 8 * 10;
+
+//-------------------------------- Divide un número entre otro con JavaScript
+
+const quotient = 66 / 33;
+
+//-------------------------------- Incrementa un número con JavaScript
+
+let myVar = 87;
+
+// Cambia solo el código debajo de esta línea
+myVar = ++myVar;
+
+//-------------------------------- Decrementa un número con JavaScri
+
+let resta = 11;
+
+// Cambia solo el código debajo de esta línea
+resta = --resta;
+
+//--------------------------------- Crea números decimales con JavaScript
+
+const ourDecimal = 5.7;
+
+// Cambia solo el código debajo de esta línea
+
+const myDecimal = 3.7;
+
+
+//---------------------------------- Multiplica dos números decimales con JavaScript
+
+const producto = 2.0 * 2.5;
+
+//---------------------------------- Divide un decimal entre otro con JavaScript
+
+const quotiento = 4.4 / 2.0; // Cambia esta línea
+
+//---------------------------------- Encuentra un resto en JavaScript
+
+const remainder = 11 % 3;
+
+//--------------------------------- Asignación compuesta con adición aumentada
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Cambia solo el código debajo de esta línea
+a += 12;
+b += 9;
+c += 7;
+
+//--------------------------------- Asignación compuesta con resta aumentada
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Cambia solo el código debajo de esta línea
+a -= 6;
+b -= 15;
+c -= 1;
+
+//-------------------------------- Asignación compuesta con multiplicación aumentada
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Cambia solo el código debajo de esta línea
+a *= 5;
+b *= 3;
+c *= 10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+//--------------------------------- Asignación compuesta con división aumentad
+
+// Cambia solo el código debajo de esta línea
+a /= 12;
+b /= 4;
+c /= 11;
+
+//--------------------------------- Escapa comillas literales en cadenas
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Cambia esta línea 
+
+//--------------------------------- Cita cadenas con comillas simples
+
+const myString = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//--------------------------------- Escapa secuencias en cadenas
+
+const myStringTwo = "FirstLine\n\t\\SecondLine\nThirdLine"; // Cambia esta línea
+
+//-------------------------------- Concatena cadenas con el operador "más"
+
+const myStringTree = "This is the start." + " This is the end."; // Cambia esta línea
+
+//-------------------------------- Concatena cadenas con el operador "más igual"
+
+let myStringFour = "This is the first sentence.";
+myStringFour += " This is the second sentence."
+
+//-------------------------------- Construye cadenas con variables
+
+// Cambia solo el código debajo de esta línea
+const myName = "Samuel";
+const myStringFive = "La persona" + myName +" Es un hombre";
+
+//---------------------------------- Agrega variables a cadenas
+
+// Cambia el código debajo de esta línea
+const someAdjective = "Cool";
+let myStringSix = "Learning to code is ";
+myStringSix += someAdjective;
+
+//---------------------------------- Encuentra la longitud de una cadena
+
+// Configuración
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+lastNameLength = lastName.length;
+
+//--------------------------------- Utiliza la notación de corchetes para encontrar el primer carácter en una cadena
+
+// Configuración
+let firstLetterOfLastName = "";
+const lastNameTwo = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+firstLetterOfLastName = laslastNameTwotName[0]; // Cambia esta línea
+
+//--------------------------------- Comprende la inmutabilidad de las cadenas
+
+// Configuración
+let myStringSeven = "Jello World";
+
+// Cambia solo el código debajo de esta línea
+myStringSeven = "Hello World"; // Cambia esta línea
+// Cambia solo el código encima de esta línea
+
+// ---------------------------------- Utiliza la notación de corchetes para encontrar el enésimo carácter en una cadena
+
+
+// Configuración
+const lastNameTree = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+const thirdLetterOfLastName = lastNameTree[2]; // Cambia esta línea
+
+
+//----------------------------------- Utiliza la notación de corchetes para encontrar el último carácter en una cadena
+
+// Configuración
+const lastNameFour = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+const lastLetterOfLastName = lastNameFour[lastNameFour.length - 1]; // Cambia esta línea
+
+
+//------------------------------------ Utiliza la notación de corchetes para encontrar el carácter enésimo final en una cadena
+
+// Configuración
+const lastNameFive = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+const secondToLastLetterOfLastName = lastNameFive[lastNameFive.length - 2];; // Cambia esta línea
+
+
+//------------------------------------- Palabra en blanco
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Cambia solo el código debajo de esta línea
+const wordBlanks = myNoun + " " + myAdjective + " " + myVerb + " " +myAdverb; // Cambia esta línea
+// Cambia solo el código encima de esta línea
+
+//------------------------------------- Almacena múltiples valores en una variable utilizando los arreglos de JavaScript
+
+// Cambia solo el código debajo de esta línea
+const myArray = ['Soy una cadena', 12];
+
+//------------------------------------- Anida un arreglo dentro de otro arreglo
+
+// Cambia solo el código debajo de esta línea
+const myArrayTwo = [[]];
+
+//------------------------------------- Accede a los datos de un arreglo con índices
+
+const myArrayTree = [50, 60, 70];
+var myData = myArrayTree[0];
+
+//------------------------------------ Modifica los datos de un arreglo con índices
+
+// Configuración
+const myArrayFour = [18, 64, 99];
+
+// Cambia solo el código debajo de esta línea
+
+myArrayFour[0] = 45;
+
+//---------------------------------- Accede a arreglos multidimensionales con índices
+
+const myArrayFive = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+const myData = myArrayFive[2][1];
+
+
+//----------------------------------- Manipula arreglos con push()
+
+// Configuración
+const myArrayEigth = [["John", 23], ["cat", 2]];
+
+// Cambia solo el código debajo de esta línea
+
+myArrayEigth.push(["dog", 3]) 
+
+//---------------------------------- Manipula arreglos con pop()
+
+// Configuración
+const myArrayNine = [["John", 23], ["cat", 2]];
+
+// Cambia solo el código debajo de esta línea
+
+const removedFromMyArray  = myArrayNine.pop();
+
+//---------------------------------- Manipula arreglos con shift()
+
+// Configuración
+const myArrayTen = [["John", 23], ["dog", 3]];
+
+// Cambia solo el código debajo de esta línea
+
+const removedFromMyArrayTwo = myArrayTen.shift();
+
+//--------------------------------- Manipula arreglos con unshift()
+
+// Configuración
+const myArrayEleven = [["John", 23], ["dog", 3]];
+myArrayEleven.shift();
+myArrayEleven.unshift(["Paul", 35]);
+
+// Cambia solo el código debajo de esta línea
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
